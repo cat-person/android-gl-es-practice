a8cd20375b9dc3a083fa51c49bed78fa7d1b601f
Aleksei Klyshnniov <alexey.klyshnikov@gmail.com>
2024-03-08 17:16:46 +0400

    Added propper build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,13 @@
 buildscript {
-    val agp_version by extra("7.4.0")
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
+    }
+
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
